dd452ac9102ece07c644c2cc270fa7cdeb238536
Divyansh044 <divyanshbangwal@gmail.com>
2024-01-20 01:05:43 +0530

    Added new user registration feature

--- to_do_list/base/views.py
+++ to_do_list/base/views.py
@@ -1,20 +1,37 @@
 from typing import Any
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView,FormView
 from .models import Task
 from django.urls import reverse_lazy
 
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+
 class CustomLoginView(LoginView):
     template_name='base/login.html'
     fields='__all__'
     redirect_authenticated_user=True
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+class RegisterPage(FormView):
+    template_name='base/register.html'
+    form_class=UserCreationForm
+    redirect_authenticated_user=True
+    success_url=reverse_lazy('login')
+
+    def form_valid(self, form):
+        user=form.save()
+        if user is not None:
+            login(self.request(),user)
+        return super(RegisterPage,self).form_valid(form)
     
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
@@ -39,12 +56,16 @@ class TaskDetail(LoginRequiredMixin,DetailView):
 
 class TaskCreate(LoginRequiredMixin,CreateView):
     model=Task
-    fields='__all__'
+    fields=['title','description','complete']
     success_url=reverse_lazy('tasks')
+
+    def form_valid(self,form):
+        form.instances.user=self.request.user
+        return super(TaskCreate,self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin,UpdateView):
     model=Task
-    fields='__all__'
+    fields=['title','description','complete'] 
     success_url=reverse_lazy('tasks')
 
 
